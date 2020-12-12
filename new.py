ea85b813251e8b87ee40dded065c3b0fbccbb021
Anuj Shukla <68471872+anuj-moto@users.noreply.github.com>
2020-12-12 18:41:34 +0530

    Update and rename new.txt to new.py

---
+++ new.py
@@ -0,0 +1,19 @@
+#branch - new-version
+#branch 2
+#Finfing closeset value of a number in an array
+
+num=int(input("Enter the number of elements do u want:"))
+array=[]
+for i in range(0,num):
+  x=int(input("enter your element:"))
+  array.append(x)
+
+find=int(input("enter number to find closest value:"))
+results=0
+result=abs(find-array[0])
+for j in array:
+  if abs(find-j)<result:
+    result=abs(find-j)
+    results=find-j
+print("closest value is:",find-results)
+print("closest number difference:",results)
